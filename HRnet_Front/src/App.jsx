c0990284db6b3cbca8e95aa6c0b1e2c3f5922d6d
Biaguy Niakate <n.biaguy@gmail.com>
2025-03-10 14:00:06 +0100

    supression du fichier app.jsx

--- HRnet_Front/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./css/App.css";
-import { RouterProvider } from "react-router-dom";
-import {router} from "./routes/Router"
-
-
-export function App() {
-  return <RouterProvider router={router}/>;
-}
-
