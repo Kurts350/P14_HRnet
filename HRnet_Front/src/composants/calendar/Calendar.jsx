ab03f0656bb1ca9d62c4f9b40d2a14f391d1c1a3
Biaguy Niakate <n.biaguy@gmail.com>
2025-03-12 16:37:26 +0100

    nettoyage du repo

--- HRnet_Front/src/composants/calendar/Calendar.jsx
+++ HRnet_Front/src/composants/calendar/Calendar.jsx
@@ -1,37 +1,33 @@
-import styled from "styled-components"
-import PropTypes from 'prop-types';
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const CalendarInput = styled.input`
   max-width: 200px;
   padding: 8px;
   border-radius: 4px;
   border: 1px solid #ccc;
-  
+
   &:focus {
     outline: 2px solid #0078d4;
   }
-`
+`;
 
-
-export function Calendar( {id, value, onChange, required = true} ) {
+export function Calendar({ id, value, onChange, required = true }) {
   return (
-<CalendarInput
-id={id}
-type="date"
-value={value}
-onChange={onChange}
-required= {required}
-aria-required="true"
-/>
-  )
+    <CalendarInput
+      id={id}
+      type="date"
+      value={value}
+      onChange={onChange}
+      required={required}
+      aria-required="true"
+    />
+);
 }
 
 Calendar.propTypes = {
   id: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  required: PropTypes.bool
+  required: PropTypes.bool,
 };
-
-
-
